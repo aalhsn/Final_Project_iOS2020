77ede47c982f91a3b46adf2c14803456e8c7d899
Abdullah Alhasan <a.a.alhasan95@gmail.com>
2020-02-08 16:40:15 +0300

    Initial Commit

---
+++ omsi_board/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  omsi_board
+//
+//  Created by Abdullah Alhasan on 2/8/20.
+//  Copyright © 2020 Abdullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
