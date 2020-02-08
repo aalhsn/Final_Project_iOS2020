706b14ed602336d847d8d2b1d4a806cb21ed710d
HisDesigns <h.i.i.s@hotmail.com>
2020-02-08 19:51:37 +0300

    Networking Added

---
+++ Pods/Target Support Files/Pods-omsi_board/Pods-omsi_board-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_omsi_board : NSObject
+@end
+@implementation PodsDummy_Pods_omsi_board
+@end
--- omsi_board/Networking.swift
+++ omsi_board/Networking.swift
@@ -7,3 +7,71 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseAuth
+
+
+
+class Networking {
+    
+
+    //have to be static
+    static func signin (email: String, pass: String, success:  @escaping () -> (), fail: @escaping () -> () ){
+        
+        
+
+
+        Auth.auth().signIn(withEmail: email, password: pass) { (auth, error) in
+            if error == nil {
+                if auth == auth {
+                    print("signedin seccessful")
+                    success()
+                } else {
+                    //error
+                    fail()
+                }
+
+            } else {
+                print("couldnt sign in")
+                fail()
+
+            }
+        }
+
+    }
+
+
+    static func siggnup(email: String, pass: String, success:  @escaping () -> (), fail: @escaping () -> () ){
+        Auth.auth().createUser(withEmail: email, password: pass) { (auth, error) in
+            if error == nil {
+                if auth == auth {
+                    success()
+                    print("signup seccessful")
+                } else {
+                    //error
+                    fail()
+                }
+
+            } else {
+                print("couldnt sign up")
+                fail()
+            }
+        }
+    }
+
+    static func signout(seccess:  @escaping () -> (), fail: @escaping () -> () ){
+        //do something with storyboard
+        //error handeling
+        do{
+            try Auth.auth().signOut()
+            seccess()
+        }
+        catch{
+            
+            fail()
+            print("their was an error thrown!", error)
+        }
+
+    }
+    
+}
